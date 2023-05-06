dae1a4f01693112653dbe6bb9e080399715a13f5
DevOps-at-095793547586 <kimdan2030@gmail.com>
2023-05-06 09:44:13 +0300

    Spring Boot App - Heroku

--- src/main/java/com/example/dockerappapis/Controllers/IndexController.java
+++ src/main/java/com/example/dockerappapis/Controllers/IndexController.java
@@ -2,14 +2,16 @@ package com.example.dockerappapis.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
+    @RequestMapping(method = RequestMethod.GET, value = "/")
 
     public String displayPage(){
-        return "displayPage";
+        return "displayPage.html";
     }
 
 }
